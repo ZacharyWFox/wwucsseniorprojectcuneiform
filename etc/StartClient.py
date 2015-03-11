7b51afa4eed72380dbd131369ff994a1d050c551
norwejoo <zachwfox@hotmail.com>
2015-03-10 19:34:25 -0700

    One more script change

--- etc/StartClient.py
+++ etc/StartClient.py
@@ -3,8 +3,8 @@
 import os, sys, subprocess
 hostname=sys.argv[1]
 
-os.chdir("/home/jungs3/wwucsseniorprojectcuneiform/bin/JARS")
-cmd = ["java", "-cp", "*", "-Djava.rmi.server.hostname=" + hostname, "cuneiform.Experiment", "nohup"]
+os.chdir("/home/jungs3/wwucsseniorprojectcuneiform")
+cmd = ["java", "-cp", "bin/JARS/*", "-Djava.rmi.server.hostname=" + hostname, "cuneiform.Experiment", "nohup"]
 client = subprocess.Popen(cmd)
 with open("Expermient.log", "a+") as outfile:
 	while client.poll():
