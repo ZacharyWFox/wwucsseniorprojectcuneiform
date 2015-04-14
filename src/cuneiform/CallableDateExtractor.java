59f734816ff1e37ed35590bc6deadac2bcc06d1d
Zachary Fox <zachwfox@hotmail.com>
2015-04-14 11:22:20 -0700

    Removed comments

--- src/cuneiform/CallableDateExtractor.java
+++ src/cuneiform/CallableDateExtractor.java
@@ -14,6 +14,7 @@ public class CallableDateExtractor implements Callable<List<GuessPair>>{
 	SimilarityMatrix simMat;
 	List<FoundDate> sample;
 	boolean compare;
+	
 	// this extracts dates and only dates...parallelelely.
 	public CallableDateExtractor(List<KnownDate> months, List<KnownDate> years, List<FoundDate> sample, SimilarityMatrix sim) {
 		this.extranctinator = new DateExtractor(months, years);	
@@ -31,13 +32,8 @@ public class CallableDateExtractor implements Callable<List<GuessPair>>{
 	
 	@Override
 	public List<GuessPair> call() throws Exception {
-		// TODO Auto-generated method stub
-		
 		List<GuessPair> guesses = new ArrayList<GuessPair>(250);
-		
 		guesses = this.extranctinator.alignYears(this.sample, this.simMat, compare);
-//		guesses = this.extranctinator.alignYearsTest();//XXX
-		
 		return guesses;
 	}
 	
